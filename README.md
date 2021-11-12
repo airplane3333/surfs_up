4ee21fc0ab1153314ffef73ed0f482d24b504764
Ken P <87345178+airplane3333@users.noreply.github.com>
2021-11-12 16:28:27 -0700

    updated challenge file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# surfs_up
+#Overview of the analysis: 
+---
+Using data from a sqlite database that contains temprature for a date from serveral reporting 
+station within Hawaii.  The data is used to perform statictical analysis for a give month to decide
+if the weather would impact opening of an ice cream and surf shop on the island of oahu. 
+##Results: 
+---
+Provide a bulleted list with three major points from the two analysis deliverables. Use images as 
+support where needed.
+
+![June Statistical Weather Data](june_stats.png)
+
+
+![December Statistical Weather Data](dec_stats.png)
+
+##Summary: 
+---
+Provide a high-level summary of the results and two additional queries that you would perform to 
+gather more weather data for June and December.
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 #This connect python to sqlite db
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine('sqlite:///hawaii.sqlite')
 #This reflects the data 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -23,12 +23,12 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
+    Welcome to the Climate Analysis API!<bf/>
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end<br/>
     ''')
 #adding a new route for FLASK
 @app.route("/api/v1.0/precipitation")
@@ -52,3 +52,17 @@ def temp_monthly():
         filter(Measurement.date >= prev_year).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel= [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    
+    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
